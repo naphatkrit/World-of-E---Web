5b554d573da85725365d0f14a6770518b0d980c8
nathanshiaulam <nlam@princeton.edu>
2014-03-01 17:57:00 -0500

    Added cgi

--- NateAndEvelyn/WorldOfEBackEnd.py
+++ NateAndEvelyn/WorldOfEBackEnd.py
@@ -13,32 +13,25 @@ collection_all_data = db.world_of_e_all_data
 
 def store_data():
     cell_information = json.loads(open("All.json").read())
-
+    print "hello world";
     # below arrays are for checking whether or not something already exists.
     # not required until adding functions are used.
-    class_names = []
-    workshops = []
-    competitions = []
-    funding = []
-    internships = []
-    clubs = []
-    trips = []
 
     #stores information into database
-    collection_learn.insert(cell_information[0])
-    collection_do.insert(cell_information[1])
-    collection_connect.insert(cell_information[2])
-    collection_all_data.insert(cell_information)
+    collection_learn.insert(cell_information[0]);
+    collection_do.insert(cell_information[1]);
+    collection_connect.insert(cell_information[2]);
+    collection_all_data.insert(cell_information);
 
 
 def exportData(): 
-	data = []
+	data = [];
 	for section in collection_all_data.find():
 		for content in section.get('sections'):
 	 		for stuff in content.get('content'):
  	 			if stuff.get('url') is not None:
-	 				fixed_URL = stuff.get('url')
-	 				stuff['url'] = fixed_URL
+	 				fixed_URL = stuff.get('url');
+	 				stuff['url'] = fixed_URL;
 	 			if stuff.get('details') is not None:
 	 				fixed_details = stuff.get('details')
 	 				stuff['details'] = fixed_details
