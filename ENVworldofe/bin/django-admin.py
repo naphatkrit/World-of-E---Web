76e8c1cbf4f72e31f93fd1f7e47831e01df433fc
Naphat Sanguansin <naphat.krit@gmail.com>
2014-03-17 12:40:29 -0400

    Set up django properly with apache

--- ENVworldofe/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/shared/dj_worldofe/ENVworldofe/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- worldofe/worldofe/settings.py
+++ worldofe/worldofe/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '00j!sk63k8*&i)y8lw&r)f*$zz9m_(_)^n&toxdbba2%z+ez7c'
+SECRET_KEY = 's555#gd2+e^l(e@+gg2s($sggu99+1s*+&a^w7u%l=y5d(j+tp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
