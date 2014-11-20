ae496c71af230b3f73c2119e97a825c4dc195189
naphatkrit <naphat.krit@gmail.com>
2014-11-19 19:13:36 -0500

    updated to use postgres

--- worldofe/settings/common.py
+++ worldofe/settings/common.py
@@ -17,7 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm71cifkwsz&8fw4^xx*chbquindp9hh54*ygy^0rb7_)243t+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = []
--- worldofe/settings/prod.py
+++ worldofe/settings/prod.py
@@ -1,16 +1,22 @@
 from common import *
-
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+#DEBUG = bool(environ.get('DJANGO_DEBUG', ''))
 DEBUG = False
 
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
+# ########## END DEBUG CONFIGURATION
 
-DATABASES = {
-    'default': {
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME': 'worldofe_db',
-        'USER': 'worldofe_admin',
-        'PASSWORD': '#Ps12Ei34'
-    }
-}
+########## DATABASE CONFIGURATION
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-SECRET_KEY = 'lkjsd@#REAFWsdfija$^WTAREGFDZL#QJ$5wreaFTWJ$%LK&^U&JRTYR'
+########## END DATABASE CONFIGURATION
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECRET_KEY = environ.get('DJANGO_SECRET_KEY', '')
+
